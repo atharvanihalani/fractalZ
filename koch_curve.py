a378375edcaad74699f2fb8db81aa79ed63c5aef
atharvanihalani <113245246+atharvanihalani@users.noreply.github.com>
2023-05-20 17:26:38 -0400

    added couple cases & cleaned up code

--- koch_curve.py
+++ koch_curve.py
@@ -1,55 +1,96 @@
 import turtle
 
 mt = turtle.Turtle()
+mt.hideturtle()
+mt.speed(0)
 mt.pu()
-mt.goto(-200, 0)
+mt.goto(-350, 0)
 mt.pd()
 
-x = 10 # length
-iter = 4 # iterations
-angle = 60 # the angle at da tippy top
 
-def regKoch(val: int):
-    if val == 0:
+def reg_koch_1(iter: int, len: float, angle: float):
+    '''
+    initialize wiith start length \r\n
+    angle is constant height w varying base size
+    '''
+
+    if iter == 0:
         return
     
-    regKoch(val-1)
+    reg_koch_1(iter-1, len, angle)
     mt.lt(90-(angle/2))
-    mt.fd(x)
-    regKoch(val-1)
+    mt.fd(len)
+    reg_koch_1(iter-1, len, angle)
     mt.rt(180-angle)
-    mt.fd(x)
-    regKoch(val-1)
+    mt.fd(len)
+    reg_koch_1(iter-1, len, angle)
     mt.lt(90-(angle/2))
-    mt.fd(x)
-    regKoch(val-1)
+    mt.fd(len)
+    reg_koch_1(iter-1, len, angle)
 
-# mt.fd(x)
-# regKoch(iter)
+def reg_koch_2(iter: int, len: int, angle: float):
+    '''
+    iter index is 'forward shifted' compared to the prev method \r\n
+    again, angle is constant length w varying base size
+    '''
+    reg_koch_2_helper(iter, len, angle)
+    mt.lt(90-(angle/2))
+    reg_koch_2_helper(iter, len, angle)
+    mt.rt(180-angle)
+    reg_koch_2_helper(iter, len, angle)
+    mt.lt(90-(angle/2))
+    reg_koch_2_helper(iter, len, angle)
 
+def reg_koch_2_helper(iter: int, len: float, angle: float):
+    if iter == 0:
+        mt.fd(len)
+    else:
+        reg_koch_2(iter-1, len, angle)
 
-def skewKoch(val: int):
-    if val == 0:
+# 'skews' the curve at the lowest recursive level
+def faux_koch_1(iter: int, len: int, angle: float):
+    if iter == 0:
         return
     
-    skewKoch(val-1)
+    faux_koch_1(iter-1, len, 60)
+    mt.fd(len)
     mt.lt(90-(angle/2))
-    mt.fd(x)
-    skewKoch(val-1)
+    faux_koch_1(iter-1, len, 60)
+    mt.fd(len)
     mt.rt(180-angle)
-    mt.fd(x)
-    skewKoch(val-1)
+    faux_koch_1(iter-1, len, 60)
+    mt.fd(len)
     mt.lt(90-(angle/2))
-    mt.fd(x/2)
-    skewKoch(val-1)
+    faux_koch_1(iter-1, len, 60)
+    mt.fd(len)
 
-mt.fd(x)
-skewKoch(iter)
+# koch curve but skewed (and hence, scaled slightly diff)
+def skew_koch(iter: int, len: float, angle: float):
+    '''
+    initialize w start length x
+    '''
+
+    if iter == 0:
+        return
+    
+    skew_koch(iter-1, len, angle)
+    mt.lt(90-(angle/2))
+    mt.fd(len/2)
+    skew_koch(iter-1, len/2, angle)
+    mt.rt(180-angle)
+    mt.fd(len/2)
+    skew_koch(iter-1, len/2, angle)
+    mt.lt(90-(angle/2))
+    mt.fd(len/2)
+    skew_koch(iter-1, len/2, angle)
+
+mt.fd(50)
+skew_koch(3, 50, 60)
 
 
 # koch w same (triangle) base, that gets extended to be pointier => will intersect
-
-
+# infinitely generating koch curve (unbounded recursion)
+# defined area (rather than defined side length) => pass x/2 as recursive param
 
 
 
